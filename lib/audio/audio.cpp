c117d8656c33b06cf4f44565460aa85920dcc1c5
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2021-03-05 18:24:14 -0800

    added mini audio input library

---
+++ lib/audio/audio.cpp
@@ -0,0 +1,131 @@
+#include "audio.h"
+
+#define USE_SERIAL 1
+
+#ifdef USE_SERIAL
+#include <Arduino.h>
+#endif
+
+uint16_t gSamples[2][kSamplesPerMeasurement];
+uint16_t volatile gMeasurementsCount = 0;
+uint8_t volatile gCurrentBuffer = 0;
+
+void audio_error(uint8_t code) {
+// Not sure what to do if we do not have serial line, so just stop everything
+#ifdef USE_SERIAL
+  Serial.print("Audio Error: ");
+  Serial.println(code);
+#endif
+  while (1)
+    ;
+}
+  
+  
+#if 0
+  ADMUX |= (1 << REFS0); //set reference voltage
+  ADMUX |= (1 << ADLAR); //left align the ADC value- so we can read highest 8 bits from ADCH register only
+  
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS0); //set ADC clock with 32 prescaler- 16mHz/32=500kHz
+  ADCSRA |= (1 << ADATE); //enabble auto trigger
+  ADCSRA |= (1 << ADIE); //enable interrupts when measurement complete
+  ADCSRA |= (1 << ADEN); //enable ADC
+  ADCSRA |= (1 << ADSC); //start ADC measurements
+ISR(ADC_vect) {//when new ADC value ready
+  incomingAudio = ADCH;//update the variable incomingAudio with new value from A0 (between 0 and 255)
+}
+#endif
+
+void configure_audio(uint8_t adcChannel, uint8_t clockPrescaler) {
+  cli();  // Disable interupts
+
+  ADCSRA = 0;  // clear ADCSRA register
+  ADCSRB = 0;  // clear ADCSRB register
+
+  if (adcChannel > 7) {
+    audio_error(1);
+  }
+  ADMUX = adcChannel & 0x07;  // set analog input pin
+  ADMUX |= (1 << REFS0);      // set reference voltage
+
+  uint32_t clock = F_CPU / clockPrescaler;
+  if (clock > 200000) {
+    // Future work: use 8 bits only for high sampling frequencies
+    ADMUX |= (1 << ADLAR);  // left align ADC value to 8 bits from ADCH register
+    audio_error(2);
+  }
+
+  switch (clockPrescaler) {
+    case 2:
+      ADCSRA |= (1 << ADPS0);
+      break;
+    case 4:
+      ADCSRA |= (1 << ADPS1);
+      break;
+    case 8:
+      ADCSRA |= (1 << ADPS1) | (1 << ADPS0);
+      break;
+    case 16:
+      ADCSRA |= (1 << ADPS2);
+      break;
+    case 32:
+      ADCSRA |= (1 << ADPS2) | (1 << ADPS0);
+      break;
+    case 64:
+      ADCSRA |= (1 << ADPS2) | (1 << ADPS1);
+      break;
+    case 128:
+      ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+      break;
+    default:
+      audio_error(3);
+  }
+
+  ADCSRA |= (1 << ADATE);  // enable auto trigger
+  ADCSRA |= (1 << ADIE);   // enable interrupts when measurement complete
+  ADCSRA |= (1 << ADEN);   // enable ADC
+  ADCSRA |= (1 << ADSC);   // start ADC measurements
+
+  sei();  // Enable interrupts
+}
+
+float compute_rms(const uint8_t buffer) {
+  uint16_t *ptr = kBufferHead[buffer];
+  uint32_t sqsum = 0;
+  while (ptr < kBufferEnd[buffer]) {
+    int16_t val = *(ptr++) - 512;
+    sqsum += val * val;
+  }
+
+  return sqrt((float)sqsum / (float)kSamplesPerMeasurement);
+}
+
+ISR(ADC_vect) {
+  static uint8_t *byteSlot = kSamplesBuffer1;
+  *(byteSlot++) = ADCL;
+  *(byteSlot++) = ADCH;
+  
+  if (byteSlot < kSamplesBuffer2) {
+    gCurrentBuffer = 0;
+  } else {
+    gCurrentBuffer = 1;
+  }
+  if (byteSlot >= kSamplesEnd) {
+    byteSlot = kSamplesBuffer1;
+  }
+  gMeasurementsCount++;
+}
+
+/*
+avr-g++ -E -c -fno-exceptions -fno-threadsafe-statics -fpermissive -std=gnu++11
+-Os -Wall -ffunction-sections -fdata-sections -flto -mmcu=atmega328p
+-DPLATFORMIO=50100 -DARDUINO_AVR_PROTRINKET3FTDI -DF_CPU=12000000L
+-DARDUINO_ARCH_AVR -DARDUINO=10808 -DUNIT_TEST -DUNITY_INCLUDE_CONFIG_H
+-Ilib/audio
+-I/home/itdad/.platformio/packages/framework-arduino-avr/cores/arduino
+-I/home/itdad/.platformio/packages/framework-arduino-avr/variants/eightanaloginputs
+-I.pio/build/pt3/UnityTestLib -I/home/itdad/.platformio/packages/tool-unity
+lib/audio/audio.cpp > out.cpp
+
+extern "C" void __vector_21 (void) __attribute__ ((signal,used,
+externally_visible)) ; void __vector_21 (void) { ... }
+*/
---
+++ lib/audio/audio.h
@@ -0,0 +1,41 @@
+#ifndef SOUNDLEVEL_AUDIO_H
+#define SOUNDLEVEL_AUDIO_H
+
+#include <Arduino.h>
+#include <stdint.h>
+
+// The ATmega238P conversion takes 13 clock cycles
+// The Trinket Pro 3.3V runs at 12MHz
+// with prescaler = 128 --> sampling rate = 12E6 / 128 / 13 ~= 7.2 kHz
+const uint32_t kAdcClockPrescaler = 128;
+const uint32_t kAdcAcquisitionCycles = 13;
+const uint32_t kSampleFrequency =
+    F_CPU / kAdcClockPrescaler / kAdcAcquisitionCycles;
+
+// Collection of sound envelope over 40ms (25Hz)
+const uint16_t kMeasurementPeriodMs = 40;
+
+// Rounded samples required for one measurement
+// (watch order the order of operations to make sure there is no rounding error
+// (32 -> 16 bits). With the settings above, it should result to 288 samples.
+const uint16_t kSamplesPerMeasurement =
+    kSampleFrequency * kMeasurementPeriodMs / 1000;
+
+extern uint16_t gSamples[2][kSamplesPerMeasurement];
+extern volatile uint16_t gMeasurementsCount;
+extern volatile uint8_t gCurrentBuffer;
+
+uint8_t* const kSamplesBuffer1{reinterpret_cast<uint8_t*>(gSamples[0])};
+uint8_t* const kSamplesBuffer2{
+    reinterpret_cast<uint8_t*>(gSamples[0] + kSamplesPerMeasurement)};
+uint8_t* const kSamplesEnd{
+    reinterpret_cast<uint8_t*>(gSamples[0] + 2 * kSamplesPerMeasurement)};
+uint16_t* const kBufferHead[2] = {gSamples[0], gSamples[1]};
+uint16_t* const kBufferEnd[2] = {gSamples[1],
+                                 gSamples[0] + 2 * kSamplesPerMeasurement};
+
+void configure_audio(uint8_t adcChannel, uint8_t clockPrescaler = 128);
+
+float compute_rms(const uint8_t buffer);
+
+#endif
